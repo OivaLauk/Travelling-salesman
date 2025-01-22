41333554511470a3aab8e2151923cbb14679afce
OivaLauk <oiva.laukkanen@edu.salpaus.fi>
2025-01-22 22:39:06 +0200

    päivitys

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const { checkPrimeSync } = require('crypto');
 const fs = require('fs');
 fs.readFile('./kaupungit.json', function(err, data) { 
     if (err) throw err; 
@@ -7,8 +6,9 @@ fs.readFile('./kaupungit.json', function(err, data) {
     const radius = 6371;
     const startingCity = json.cities[0]
     const nextCity = json.cities[1]
-
-    console.log(calculateDistance(startingCity.city, nextCity.city, startingCity.latitude, nextCity.latitude, startingCity.longitude, nextCity. longitude))
+    const otherCities = json.shift()
+    console.log(otherCities)
+    console.log(calculateDistance(startingCity.name, nextCity.name, startingCity.latitude, nextCity.latitude, startingCity.longitude, nextCity. longitude))
 
     function calculateDistance(city1, city2, lat1, lat2, lon1, lon2) {
         const pi = Math.PI;
@@ -19,14 +19,15 @@ fs.readFile('./kaupungit.json', function(err, data) {
         const dLat = lat2Rad - lat1Rad;
         const dLon = lon2Rad - lon1Rad;
 
-        const x1 = Math.pow(Math.sin(dLat/2), 2) + Math.cos(lat1Rad);
-        const x2 = Math.cos(lat2Rad) * Math.pow(Math.sin(dLon/2), 2);
-        const x3 = 2 * Math.asin(Math.sqrt(x1 * x2));
-        const x4 = Math.pow(Math.sin(Math.sqrt(x3)), -1);
-        const x5 = x4 * (pi/180);
-        console.log(x1, x2, x3, x4, x5)
-        console.log(dLat)
-        return x5 * radius;
+        const x1 = Math.pow(Math.sin(dLat/2), 2) + Math.cos(lat1Rad)
+        * Math.cos(lat2Rad) * Math.pow(Math.sin(dLon/2), 2);
+
+        const x2 = 2 * Math.atan2(Math.sqrt(x1), Math.sqrt(1 - x1));
+
+        const x3 = Math.round(x2 * radius * 100) / 100;
+
+        const result = `Matka kaupungista: ${city1} kaupunkiin: ${city2} on ${x3} kilometriä.`
+        return result;
 
     }
     }

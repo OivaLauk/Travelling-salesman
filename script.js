f11d1286ea2069a68fbe68001cc529988c6aed31
OivaLauk <oiva.laukkanen@edu.salpaus.fi>
2025-01-27 14:53:47 +0200

    -

--- script.js
+++ script.js
@@ -2,49 +2,26 @@ const fs = require('fs');
 fs.readFile('./kaupungit.json', function(err, data) { 
     if (err) throw err; 
     const json = JSON.parse(data);
-    
-    const radius = 6371;
 
-    let distanceArr = [];
-    let totalDistance = 0;
-    /*
-    setTimeout(() => {
-        distanceArr.forEach((num) => totalDistance += num);
-        console.log(totalDistance);
-    }, 100);
-    */
-    let currentCity = json.cities[0]
-    let nextCity = json.cities[1]
+    const start = json.cities[0];
+    const others = json.cities.filter((city) => city.name != "Helsinki")
 
-
-    //Function for moving between cities, helsinki => espoo, espoo => tampere etc.
-    function moveCities(json) {
-        let pairs = [];
-        for (let i = 1; i < json.cities.length; i++) {
-            let city1 = json.cities[i - 1];
-            let city2 = json.cities[i];
-            pairs.push([city1, city2]);
-        }
-        if (json.cities.length > 1) {
-            pairs.push([json.cities[json.cities.length - 1], json.cities[0]]);
-        }
-        return pairs;
-    }
-    let cityPairs = moveCities(json);
-    
-cityPairs.forEach(pair => {
-  pair.forEach(city => {
-    //console.log(`Name: ${city.name}, Latitude: ${city.latitude}, Longitude: ${city.longitude}`);
-  });
-});
+    others.forEach(city => {
+        console.log(calculateDistance(start, city))
+    });
 
     //Funktio joka laskee kaupungin etäisyyden toiseen kaupunkiin käyttämällä haversine-kaavaa
-    function calculateDistance(city1, city2, lat1, lat2, long1, long2) {
+    function calculateDistance(start, others) {
+        const radius = 6371;
+
+        const city1 = start;
+        const city2 = others;
+
         const pi = Math.PI;
-        const lat1Rad = lat1 * (pi/180);
-        const lat2Rad = lat2 * (pi/180);
-        const long1Rad = long1 * (pi/180);
-        const long2Rad = long2 * (pi/180);
+        const lat1Rad = city1.latitude * (pi/180);
+        const lat2Rad = city2.latitude * (pi/180);
+        const long1Rad = city1.longitude * (pi/180);
+        const long2Rad = city2.longitude * (pi/180);
         const dLat = lat2Rad - lat1Rad;
         const dLong = long2Rad - long1Rad;
         
@@ -55,8 +32,7 @@ cityPairs.forEach(pair => {
         
         const x3 = Math.round(x2 * radius * 100) / 100;
         
-        const result = `The distance from: ${city1} to: ${city2} is ${x3} kilometers.`
-        distanceArr.push(x3);
+        const result = `The distance from: ${city1.name} to: ${city2.name} is ${x3} kilometers.`
         return result;
     }
 }

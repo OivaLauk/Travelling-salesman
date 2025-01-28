cc0eb246dd7a4896b35b2fb3aadf45ce7b71217a
OivaLauk <oiva.laukkanen@edu.salpaus.fi>
2025-01-28 21:52:31 +0200

    -

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
-const { inflate } = require('zlib');
+const { writeFile } = require('fs/promises');
 fs.readFile('./kaupungit.json', function(err, data) { 
     if (err) throw err; 
     const json = JSON.parse(data);
 
     //Funktio joka laskee kaupungin etäisyyden toiseen kaupunkiin käyttämällä haversine-kaavaa
-    function calculateDistance(start, other) {
+    function calculateDistance(node1, node2) {
         const radius = 6371;
 
-        const city1 = start;
-        const city2 = other;
+        const city1 = node1;
+        const city2 = node2;
 
         const pi = Math.PI;
         const lat1Rad = city1.latitude * (pi/180);
@@ -28,9 +28,6 @@ fs.readFile('./kaupungit.json', function(err, data) {
         return x3;
     }
 
-const INF = Infinity;
-
-let tourDist = [];
 let matrix = [];
 let rows = 10;
 let columns = 10;
@@ -44,18 +41,41 @@ for (let i = 0; i < rows; i++) {
     }
 }
 
-function rowSum(arr) {
-    for (let i = 0; i < rows; ++i) {
-        let rowSum = 0;
-        for (let j = 0; j < columns; ++j) {
-            rowSum += arr[i][j];
+
+let boolArr = [false, false, false, false, false, false, false, false, false, false];
+let shortestDistance = Infinity;
+let fullPath = [];
+
+//Brute-force algorithmi TSP:n ratkaisemiseen
+function TSP(matrix, v, currPos, n, count, cost) {
+    
+    if (count == n && matrix[currPos][0]) {
+        if(cost + matrix[currPos][0] < shortestDistance) {
+            shortestDistance = Math.min(shortestDistance, cost + matrix[currPos][0]);
         }
-        tourDist.push(Math.round(rowSum * 100) / 100);
-        matrix[i][i] = INF
     }
-    return tourDist;
+    
+    for (let i = 0; i < n; i++) {
+        if (!v[i] && matrix[currPos][i]) {
+            fullPath.push(json.cities[i].name)
+            v[i] = true;
+            TSP(matrix, v.clone(), i, n, count + 1, cost + matrix[currPos][i]);
+
+            v[i] = false;
+        }
+    }
+    return shortestDistance;
 }
-rowSum(matrix);
+
+Array.prototype.clone = function() {
+    return this.slice(0);
+};
+
+const result = Math.round(TSP(matrix, boolArr, 0, 10, 0, 0) * 100) / 100;
+
+console.log(fullPath)
+
+console.log(`Lyhyin reitti on ${result}km pitkä.`);
 
 }
 );
